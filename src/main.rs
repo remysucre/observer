73b2a954d962371862d0460af554613400df0f5a
Remy Wang <remy.sucre@gmail.com>
2019-07-19 10:51:20 -0700

    observable stuff

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+pub struct Observable<T> {
+    subscribers: Vec<Box<dyn Fn(&T) + Send>>,
+}
+
+impl<T> Observable<T> {
+    pub fn new() -> Observable<T> {
+        Observable {
+            subscribers: Vec::new(),
+        }
+    }
+
+    pub fn subscribe<F>(&mut self, callback: F)
+    where
+        F: 'static + Fn(&T) + Send,
+    {
+        self.subscribers.push(Box::new(callback));
+    }
+
+    pub fn notify(&self, item: T) {
+        for callback in &self.subscribers {
+            callback(&item);
+        }
+    }
+}
+
+fn observer2(item: &usize) {
+    println!("another {:?}", *item)
+}
+
+fn observer1(item: &usize) {
+    let mut observable2 = Observable::new();
+    observable2.subscribe(observer2);
+    observable2.notify(*item * 2);
+}
+
+fn main() {
+    let mut observable1 = Observable::new();
+    observable1.subscribe(observer1);
+    observable1.notify(5);
+}

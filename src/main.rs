56ba6920fcbe0da00d4c6a332576e37217d7e170
Remy Wang <remy.sucre@gmail.com>
2019-07-22 13:55:43 -0700

    Try the interface on String type

--- src/main.rs
+++ src/main.rs
@@ -23,37 +23,42 @@ impl<T> Observable<T> {
     }
 }
 
-fn sink1(item: &usize) {
+fn sink1(item: &String) {
     println!("Sink 1 got {:?}", *item)
 }
 
-fn sink2(item: &usize) {
+fn sink2(item: &String) {
     println!("Sink 2 got {:?}", *item)
 }
 
-fn server1(item: &usize) {
+fn server1(item: &String) {
     let mut result = Observable::new();
     result.subscribe(sink1);
     result.subscribe(sink2);
-    result.notify(*item * 2);
+    result.notify(item.clone());
 }
 
-fn server2(item: &usize) {
+fn server2(item: &String) {
     let mut result = Observable::new();
     result.subscribe(sink1);
     result.subscribe(sink2);
-    result.notify(*item * 4);
+    result.notify(item.clone());
 }
 
 fn main() {
     let mut source1 = Observable::new();
     let mut source2 = Observable::new();
 
-    source1.subscribe(server1);
+    source1.subscribe(|item: &String| {
+        let mut result = Observable::new();
+        result.subscribe(sink1);
+        result.subscribe(sink2);
+        result.notify(item.clone());
+    });
     source2.subscribe(server1);
     source1.subscribe(server2);
     source2.subscribe(server2);
 
-    source1.notify(1);
-    source2.notify(2);
+    source1.notify("1".to_string());
+    source2.notify("2".to_string());
 }
